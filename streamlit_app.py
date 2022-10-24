9f5ff7be9da84ef7a1b3c60d598cfc4f28cf695a
Matheus Guimarães <100164786+garotodeti@users.noreply.github.com>
2022-10-24 13:26:03 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,8 @@ import streamlit
 streamlit.title('My parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.header('Omega 3 & Blueberry Oatmeal')
-streamlit.header('Kale, Spinach & Rocket Snoothie')
-streamlit.header('Hard-Boiled Free-Range Egg')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Snoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
 
