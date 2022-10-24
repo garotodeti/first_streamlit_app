4868682ef730550e3e00a985d49e1745af5861a9
Matheus Guimarães <100164786+garotodeti@users.noreply.github.com>
2022-10-24 13:24:42 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
 import streamlit
 
 streamlit.title('My parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.header('Omega 3 & Blueberry Oatmeal')
+streamlit.header('Kale, Spinach & Rocket Snoothie')
+streamlit.header('Hard-Boiled Free-Range Egg')
+
+
